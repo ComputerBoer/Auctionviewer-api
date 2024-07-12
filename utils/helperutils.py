d6555a05fbc1b2247103bf0563a8400799a047d7
Computerboer <git@ikbenhenk.nl>
2024-07-13 00:43:47 +0200

    encode print to utf-8

--- utils/helperutils.py
+++ utils/helperutils.py
@@ -2,10 +2,9 @@ from datetime import datetime, timedelta
 
 def log(value):
    
-    try: print( str(datetime.now()) + ' ' + value)
-    
+    try: 
+       print(str(datetime.now()) + ' ' + value.encode("utf-8"))
     except UnicodeEncodeError:
-        print((str(datetime.now()) + ' ' + value).encode('ascii', 'ignore').decode('ascii'))
-    return print(str(datetime.now()) + ' print error')
+      print(str(datetime.now()) + ' print error')
    
     #print(u' '.join(( str(datetime.now()), value)).encode('utf-8').strip())
