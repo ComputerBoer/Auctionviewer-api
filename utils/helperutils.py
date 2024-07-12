ab2668a75c8568ae87a8d6e6f631233e57e33e3b
Computerboer <git@ikbenhenk.nl>
2024-07-13 00:33:25 +0200

    another fix try for the logging

--- utils/helperutils.py
+++ utils/helperutils.py
@@ -1,5 +1,11 @@
 from datetime import datetime, timedelta
 
 def log(value):
-    print(u' '.join(( str(datetime.now()), value)).encode('utf-8').strip())
-    # print( str(value))
+   
+    try: print( str(datetime.now()) + ' ' + value)
+    
+    except UnicodeEncodeError:
+        print((str(datetime.now()) + ' ' + value).encode('ascii', 'ignore').decode('ascii'))
+    return print(str(datetime.now()) + ' print error')
+   
+    #print(u' '.join(( str(datetime.now()), value)).encode('utf-8').strip())
