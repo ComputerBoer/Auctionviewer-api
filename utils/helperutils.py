2da852c9740ab6463ba8a36e8ef4649b45c2c391
Computerboer <git@ikbenhenk.nl>
2024-07-13 23:42:00 +0200

    replaced single quote char by single quote

--- utils/helperutils.py
+++ utils/helperutils.py
@@ -1,10 +1,10 @@
 from datetime import datetime, timedelta
 
 def log(value):
-   
-    try: 
-       print(str(datetime.now()) + ' ' + value.encode("utf-8"))
-    except UnicodeEncodeError:
-      print(str(datetime.now()) + ' print error')
+   print(str(datetime.now()) + ' ' + value)
+    # try: 
+    #    print(str(datetime.now()) + ' ' + value.encode("utf-8"))
+    # except:
+    #   print(str(datetime.now()) + ' print error')
    
     #print(u' '.join(( str(datetime.now()), value)).encode('utf-8').strip())
