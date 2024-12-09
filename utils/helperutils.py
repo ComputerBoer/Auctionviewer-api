a734b3935fb0cfe72986ab3ce9286313b787c411
Computerboer <hallo@ikbenhenk.nl>
2024-12-09 23:39:08 +0100

    try fix non ascii errors

--- utils/helperutils.py
+++ utils/helperutils.py
@@ -1,4 +1,6 @@
 from datetime import datetime, timedelta
 
 def log(value):
+    value = value.encode('ascii', errors='ignore')
+    value = value.decode()
     print(str(datetime.now()) + ' ' + value)
