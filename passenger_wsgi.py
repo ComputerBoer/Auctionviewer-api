322f9e35225c1d6fbad0b8b2e6e61838487337b7
Computerboer <hallo@ikbenhenk.nl>
2025-08-21 23:12:28 +0200

    Reapply "updated wsgi file"
    This reverts commit d20b5bfa53179ee143378c0dbb1be603e3346904.

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,7 +1,6 @@
 from app import app as application
 
-import importlib.util
-spec = importlib.util.spec_from_file_location("wsgi", "app.py")
-wsgi = importlib.util.module_from_spec(spec)
-spec.loader.exec_module(wsgi)
-
+# import importlib.util
+# spec = importlib.util.spec_from_file_location("wsgi", "app.py")
+# wsgi = importlib.util.module_from_spec(spec)
+# spec.loader.exec_module(wsgi)
